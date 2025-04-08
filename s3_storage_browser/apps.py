ca88529e9f24081b14a88d585b4650b8676f613c
rahombus97 <chrischomicki@gmail.com>
2025-04-07 20:12:59 -0700

    Remove django as dependency in pyproject.toml

--- s3_storage_browser/apps.py
+++ s3_storage_browser/apps.py
@@ -2,6 +2,5 @@ from django.apps import AppConfig
 
 
 class StorageBrowserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 's3_storage_browser'
     verbose_name = "AppHammer S3 Storage Browser"
