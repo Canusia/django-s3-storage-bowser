093747035deaffe9eea9838c3a0fc12a34373fe0
rahombus97 <chrischomicki@gmail.com>
2025-04-07 19:54:27 -0700

    Remove label from AppConfig

--- s3_storage_browser/apps.py
+++ s3_storage_browser/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class StorageBrowserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 's3_storage_browser'
-    label = 'ah-s3-storage-browser'
+    verbose_name = "AppHammer S3 Storage Browser"
