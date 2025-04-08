6040c22c2771fbb3423955bb256b4eb96eb97046
rahombus97 <chrischomicki@gmail.com>
2025-04-07 19:29:01 -0700

    Fix name in module config

--- s3_storage_browser/apps.py
+++ s3_storage_browser/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class StorageBrowserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ah_s3_storage_browser'
+    name = 's3_storage_browser'
     label = 'ah-s3-storage-browser'
