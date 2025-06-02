bd066f0fd734b7eaa0b6b10531c6d67de20a9dd5
Nick Lyga <ndlyga@apphammer.co>
2025-06-02 12:47:54 -0400

    Update views.py

--- s3_storage_browser/views.py
+++ s3_storage_browser/views.py
@@ -22,13 +22,10 @@ def storage_browser_view(request):
 
 class AwsCredentialsApiView(views.APIView):
     def post(self, request):
-        print('request', request)
-        if getattr(settings, 'DEBUG', True):
-            access_key = os.environ.get('AWS_ACCESS_KEY_ID',None)
-            secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY',None)
-            if not access_key or not secret_key:
-                return JsonResponse({'error': 'AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY not set in environment variables'}, status=500)
-        
-        client = boto3.client('sts')
-        response = client.get_session_token()
-        return JsonResponse(response['Credentials'])
+        try:
+            client = boto3.client('sts')
+            response = client.get_session_token()
+            return JsonResponse(response['Credentials'])
+        except Exception as e:
+            msg = {"message": str(e)}
+            return JsonResponse(msg, status=500)
